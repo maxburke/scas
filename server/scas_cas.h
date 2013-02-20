790106b9494aeef4f29c514146fa687298742ee8
Max Burke <maxburke@gmail.com>
2013-02-19 16:19:04 -0800

    Added public prototype for scas_cas_contains.

--- server/scas_cas.h
+++ server/scas_cas.h
@@ -18,6 +18,9 @@ scas_cas_cache_initialize(void);
 const struct scas_cas_entry_t *
 scas_cas_read(struct scas_hash_t hash);
 
+int
+scas_cas_contains(struct scas_hash_t hash);
+
 struct scas_cas_entry_t *
 scas_cas_begin_write(struct scas_hash_t hash, size_t size);
 
