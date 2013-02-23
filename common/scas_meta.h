4499dbbe93636b04bbdd55a093734beb943d5b14
Max Burke <maxburke@gmail.com>
2013-02-22 22:09:47 -0800

    Added scas_is_directory and the first file flag.

--- common/scas_meta.h
+++ common/scas_meta.h
@@ -3,6 +3,11 @@
 
 #include "scas_base.h"
 
+enum scas_file_flags_t
+{
+    flag_is_directory = 1 << 0
+};
+
 struct scas_file_meta_t
 {
     uint64_t timestamp;
@@ -25,6 +30,12 @@ struct scas_directory_meta_t
     uint32_t num_entries;
     struct scas_hash_t parent;
 };
+
+static inline int
+scas_is_directory(int flags)
+{
+    return (flags & flag_is_directory) != 0;
+}
 
 static inline struct scas_file_meta_t *
 scas_get_file_meta_base(struct scas_directory_meta_t *directory)
